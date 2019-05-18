a4426c31613619c404d1cedcafd31288199a5bbe
HeloiseSanchez <heloise.s@hotmail.fr>
2019-05-18 19:26:44 +0200

    Update connection.php
    mise à jour: ajout vérification idUser

--- application/controllers/connection.php
+++ application/controllers/connection.php
@@ -16,8 +16,9 @@
 			$this->load->model('connection_model');
 	
 			//règles pour que le form soit validé
-			$this->form_validation->set_rules('nickname','Identifiant','trim|required|alpha_dash|encode_php_tags');
-			$this->form_validation->set_rules('password', 'Mot de passe','trim|required|alpha_dash|encode_php_tags|callback_checkPassword');
+			$this->form_validation->set_rules('email', 'E-mail','trim|required|encode_php_tags|valid_email|callback_checkNickname',array('required'=>'Veuillez indiquer votre email.'));
+			$this->form_validation->set_rules('nickname','Identifiant','trim|required|alpha_dash|encode_php_tags',array('required'=>'Veuillez indiquer votre identifiant.'));
+			$this->form_validation->set_rules('password', 'Mot de passe','trim|required|alpha_dash|encode_php_tags|callback_checkPassword',array('required'=>'Veuillez indiquer votre mot de passe.'));
 
 			
 			
@@ -29,10 +30,11 @@
 			}
 			else
 			{
-				$data['idUser']=$_POST['nickname'];
-				$this->load->view('connectionSuccessView',$data);
-			}
+				$this->input->set_cookie('user', $_POST['nickname'],24*3600, '', '/', '',null,true);
+				$this->load->view('connectionSuccessView');
+			}				
 		}
+		
 		
 		//règle de vérification du form: mot de passe correct ou non
 		public function checkPassword() {
@@ -43,11 +45,21 @@
 				return true;
 			}
 			else {
-				$this->form_validation->set_message('checkPassword','mot de passe ou identifiant incorrect');
+				$this->form_validation->set_message('checkPassword','mot de passe, email ou identifiant incorrect');
 				return false;
 			}
+		}
 			
-		
+		public function checkNickname() {
+			$log=$this->connection_model->login($_POST['email']);
+			$givedNickname = $this->input->post('nickname');
+			if ($log['idUser']==$givedNickname){
+				return true;
+			}
+			else {
+				$this->form_validation->set_message('checkNickname','mot de passe, email ou identifiant incorrect');
+				return false;
+			}											
 		}
 		
 		
