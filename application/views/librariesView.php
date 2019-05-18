09821b2ed2326911f3d8e1991b1d7612ac6467e3
HeloiseSanchez <heloise.s@hotmail.fr>
2019-05-18 19:42:29 +0200

    Update librariesView.php
    Amélioration vue

--- application/views/librariesView.php
+++ application/views/librariesView.php
@@ -7,12 +7,29 @@
 	</head>
 	
 	<body>
-		<h2>Mes bibliothèques<h2>
-		<h3><a href="<?php echo base_url("myComposers");?>"> Mes compositeurs</a></h3>
-		<img class="img-fluid" src="compositeurs.jpg" alt="compositeurs">
-		<h3><a href="<?php echo base_url("myMusics");?>"> Mes morceaux</a></h3>
-		<img class="img-fluid" src="morceaux.jpg" alt="disques sur étagères et casque audio à côté">
-		
+		<div class="container">
+			<h2>Mes bibliothèques</h2>
+			
+			<h3> Mes compositeurs</h3>
+			<img class="img-fluid" src="compositeurs.jpg" alt="compositeurs">
+			
+			<ul>
+			<?php foreach ($myComposers as $myComposer) { ?>
+				<li><a id='banane' href="<?php echo base_url("composer/getCompo/"),$myComposer->idCompo; ?>"><?php echo $myComposer->firstNameCompo,'',$myComposer->lastNameCompo;?></a></li>
+			<?php } ?>
+			</ul>
+			
+			
+			<h3>Mes morceaux</h3>
+			<img class="img-fluid" src="morceaux.jpg" alt="disques sur étagères et casque audio à côté">
+			
+			<ul>
+			<?php foreach ($myMusics as $myMusic) { ?>
+				<li><a id='banane' href="<?php echo $myMusic->linkMusic ?>" target="_blank"><?php echo $myMusic->nameMusic;?></a></li>
+			<?php } ?>
+			</ul>
+			
+		</div>
 	</body>
 </html>
 
