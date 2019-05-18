61ce3a503e11c9b3aa16d12439a8ee3c548be46c
HeloiseSanchez <heloise.s@hotmail.fr>
2019-05-18 19:30:53 +0200

    Create libCompo.php
    Nouveau model:  gère l'ajout des compositeurs et des musiques dans les librairies de l'utilisateur

---
+++ application/controllers/libCompo.php
@@ -0,0 +1,33 @@
+<?php
+class LibCompo extends CI_Controller {
+	
+	public function _construct(){
+		parent::_construct();
+		$this->load->model('compoModel');
+		$this->load->model('compoLibModel');
+		
+		$idCompo = $this->uri->segment(3);
+	}
+	
+	
+	public function addCompoFav($idCompo) {
+		$this->load->model('compoLibModel');
+		
+		if (isset($_COOKIE['user'])){
+		$data['iduser']=$_COOKIE['user'];
+		}
+		$data['idcompo']=$idCompo;
+
+		$this->compoLibModel->addCompoFav($data);
+		$this->load->view('librariesView');
+	}
+	
+	public function addMusicFav() {
+		$this->compoLibModel->addMusicFav();
+	}
+}
+
+/* End of file composer.php */
+/*Location: ./ProjetWeb/application/controllers/composer.php */
+
+?>
