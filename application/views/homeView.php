7664bcc4379706a2a3f8527af2115af850abe2d4
HeloiseSanchez <heloise.s@hotmail.fr>
2019-05-18 19:42:14 +0200

    Update homeView.php

--- application/views/homeView.php
+++ application/views/homeView.php
@@ -7,8 +7,7 @@
 	</head>
 	
 	<body>
-		<!--<img class="img-fluid" src="<?php echo base_url("/image/home2.jpg")?>" alt="violon sur partition">-->
-		<?php echo img('image/home2.jpg');?>
+		<img class="img-fluid" src="<?php echo base_url("/image/home2.jpg")?>" alt="violon sur partition">
 		<p>Bienvenue! Ce site a été réalisé dans le cadre d'un projet étudiant. Vous pouvez accéder à une liste de compositeurs classés par nationalité via l'onglet 'compositeurs'. Sur la page d'un compositeur, vous pouvez découvrir plusieurs informations le concernant et une liste de ses compositions les plus connues. En cliquant sur le nom d'une musique, vous pouvez écouter cette dernière (via Youtube). N'hésitez pas à vous inscrire et vous connecter afin d'ajouter des compositeurs et des morceaux à vos favoris. Bonne écoute!  </p>
 	</body>
 </html>
