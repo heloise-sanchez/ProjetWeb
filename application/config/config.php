dd4d94fe35d52bebab35ff866b43b1e780f7b917
HeloiseSanchez <heloise.s@hotmail.fr>
2019-05-19 13:10:29 +0200

    's'

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://projetwebsanchez.herokuapp.com/projetweb';
+$config['base_url'] = 'https://projetwebsanchez.herokuapp.com/application';
 
 /*
 |--------------------------------------------------------------------------
