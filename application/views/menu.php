b5ce80733872fd2a256b29e9e6f1694be6992021
HeloiseSanchez <heloise.s@hotmail.fr>
2019-05-20 13:07:10 +0200

    's'

--- application/views/menu.php
+++ application/views/menu.php
@@ -72,15 +72,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<li id="listComponent" class="dropdown">
 						<a href="javascript:void(0)" class="dropbtn">Compositeurs</a>
 						<div class="dropdown-content">
-							<a href="<?php echo base_url("listComposer/getCompo/Allemagne");?>">Allemagne</a>
-							<a href="<?php echo base_url("listComposer/getCompo/Autriche");?>">Autriche</a>
-							<a href="<?php echo base_url("listComposer/getCompo/France");?>">France</a>
-							<a href="<?php echo base_url("listComposer/getCompo/Italie");?>">Italie</a>
-							<a href="<?php echo base_url("listComposer/getCompo/Pologne");?>">Pologne</a>
-							<a href="<?php echo base_url("listComposer/getCompo/RepubliqueTcheque");?>">République Tchèque</a>
-							<a href="<?php echo base_url("listComposer/getCompo/Royaume-Uni");?>">Royaume-Uni</a>
-							<a href="<?php echo base_url("listComposer/getCompo/Russie");?>">Russie</a>
-							<a href="<?php echo base_url("listComposer/getCompo/Suede");?>">Suède</a>														
+							<a href="<?php echo base_url("ListComposer/getCompo/Allemagne");?>">Allemagne</a>
+							<a href="<?php echo base_url("ListComposer/getCompo/Autriche");?>">Autriche</a>
+							<a href="<?php echo base_url("ListComposer/getCompo/France");?>">France</a>
+							<a href="<?php echo base_url("ListComposer/getCompo/Italie");?>">Italie</a>
+							<a href="<?php echo base_url("ListComposer/getCompo/Pologne");?>">Pologne</a>
+							<a href="<?php echo base_url("ListComposer/getCompo/RepubliqueTcheque");?>">République Tchèque</a>
+							<a href="<?php echo base_url("ListComposer/getCompo/Royaume-Uni");?>">Royaume-Uni</a>
+							<a href="<?php echo base_url("ListComposer/getCompo/Russie");?>">Russie</a>
+							<a href="<?php echo base_url("ListComposer/getCompo/Suede");?>">Suède</a>														
 						</div>
 					</li>
 					<li id="listComponent"><a href="<?php echo base_url("inscription/create");?>">S'inscrire</a></li>
