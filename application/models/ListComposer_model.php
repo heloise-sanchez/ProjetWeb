e1ed28262f5fd45f1a18bdd010c02d47a9b0e05b
HeloiseSanchez <heloise.s@hotmail.fr>
2019-05-20 18:09:19 +0200

    'debugmodel'

--- application/models/ListComposer_model.php
+++ application/models/ListComposer_model.php
@@ -1,11 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 	class ListComposer_model extends CI_Model {
 		public function _construct(){
-			$this->load->database();
+			parent::__construct();
 		}
-		protected $table = 'composer';
+		
 
 		public function getComposers($nationality) {
+			protected $table = 'composer';
+			$this->load->database();
 			$this->db->select('idCompo,lastNameCompo');
 			$this->db->from('composer');
 			$this->db->where('nationality',$nationality);
